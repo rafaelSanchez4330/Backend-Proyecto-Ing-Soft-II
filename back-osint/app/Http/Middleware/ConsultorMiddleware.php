64ff475046f3b939980e94225ba3a7b8c7b5e164
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-12-04 11:35:02 -0600

    Add files via upload

---
+++ back-osint/app/Http/Middleware/ConsultorMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class ConsultorMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check() || Auth::user()->rol !== 'consultor') {
+            return redirect()->route('login')->with('error', 'Acceso no autorizado');
+        }
+        return $next($request);
+    }
+}
